9312fd108df8d1830189afd5eac5a35846c6458e
lllll <xzkov221@proton.me>
2022-10-14 08:46:55 +0300

    ʕ ᵔᴥᵔ ʔ

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include "search_server.h"
 #include <deque>
+#include <vector>
+#include <string>
 
 class RequestQueue {
 public:
@@ -8,33 +10,23 @@ public:
 
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+
     std::vector<Document> AddFindRequest(const std::string& raw_query);
 
     int GetNoResultRequests() const;
+
 private:
     struct QueryResult {
-        std::vector<Document> result;
-        int number;
+        uint64_t timestamp;
+        size_t results;
     };
     std::deque<QueryResult> requests_;
-    const static int min_in_day_ = 1440;
-    int curr_in_day = 0;
-    int sum_no_result = 0;
-    const SearchServer& server;
-};
+    const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
+    const static int sec_in_day_ = 1440;
 
-template <typename DocumentPredicate>
-std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-    std::vector<Document> results = server.FindTopDocuments(raw_query, document_predicate);
-    ++curr_in_day;
-    if (results.empty()) ++sum_no_result;
-    if (requests_.size() >= min_in_day_) {
-        requests_.pop_front();
-        requests_.push_back(QueryResult{ results, curr_in_day % min_in_day_ });
-        if (requests_[0].result.empty()) --sum_no_result;
-        return results;
-    }
-    requests_.push_back(QueryResult{ results, curr_in_day });
-    return results;
-}
+    void AddRequest(size_t results_num);
+};
